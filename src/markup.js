9dddea8698f07abfb0fa9ebcc9427143825e283d
splach-coder <anasbenabbou776@gmail.com>
2023-11-09 15:25:57 +0100

    complete the challenge one markup.js

---
+++ src/markup.js
@@ -0,0 +1,19 @@
+import logo from "./logo192.png";
+
+function Markup() {
+  return (
+    <div>
+      <img src={logo} alt="" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first Released in 2013</li>
+        <li>Was originlly created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of entreprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Markup;
