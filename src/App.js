7ac8727672b175dc8cee918308c47caf6f33e98d
splach-coder <anasbenabbou776@gmail.com>
2023-11-11 22:00:11 +0100

    add the tailwindcss configiguration and integrate lightmode and nightmode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <div></div>;
+  return (
+    <div className="px-6 py-3 w-fit h-50  bg-slate-500 rounded-lg dark:bg-violet-600 dark:text-white">
+      Hello
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./project1/project1";
-import "./project1/project1.css";
+import "./index.css";
+import App from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+
     <App />
   </React.StrictMode>
 );
