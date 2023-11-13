e88110d47a86ca6d77610e5cc651066a59e4bd44
splach-coder <anasbenabbou776@gmail.com>
2023-11-13 16:11:27 +0100

    edit some styles make the navbar responsive

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Card from "./components/card";
 
 export default function App() {
   return (
-    <div className="container dark:bg-[#23272F] dark:text-white font-inter">
+    <div className="dark:bg-[#23272F] dark:text-white font-inter">
       <Header />
-      <main className="py-12 flex flex-col items-center md:flex-row md:items-start md:justify-center gap-10">
+      <main className="pt-20 flex flex-col items-center md:flex-row md:items-start md:justify-center gap-10 z-[-1]">
         <Card />
         <Card />
         <Card />
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,18 @@
 import logo from "../imgs/logo192.png";
 import Burger from "../svg/burger.svg";
 import ThemeSwitcher from "./ThemeSwitcher";
+import React, { useState } from "react";
 
 export default function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
-    <header className="w-100 flex items-center md:justify-between py-3 px-6">
-      <div className="burger cursor-pointer md:hidden">
+    <header className="w-screen flex items-center md:justify-between py-3 px-6 fixed bg-white shadow-md dark:bg-[#23272F] z-[1]">
+      <div className="burger cursor-pointer md:hidden" onClick={toggleNav}>
         <img src={Burger} alt="" className="w-[35px] me-3" />
       </div>
       <div className="logo flex items-center gap-3 text-primary">
@@ -17,8 +24,11 @@ export default function Header() {
         <h2>ReactFacts</h2>
       </div>
 
-      <nav>
-        <ul className="hidden md:flex gap-12 text-xl ">
+      <nav
+        className={`md:block fixed md:relative top-16 md:top-0 left-0 px-12 z-50 md:bg-transparent shadow-lg h-screen transition-all ease-in-out duration-250 md:h-auto md:shadow-none bg-white dark:bg-[#23272F] ${
+          isNavOpen ? "block" : "hidden"
+        } `}>
+        <ul className="flex flex-col md:flex-row gap-3 py-6 md:py-0 md:gap-12 text-xl ">
           <li className="cursor-pointer transition ease-in duration-75 hover:border-b-2 border-primary pb-2">
             Home
           </li>
